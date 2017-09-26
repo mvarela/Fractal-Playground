91c05b63412c5e864ad2634adec01af9b31227f5
Martín Varela <martin@varela.fi>
2017-09-26 08:56:14 +0300

    Math/abs can't cope with ratios, apparently.
    Cast the corrected iteration count to double, now it works

--- src/fractals/mandelbrot.clj
+++ src/fractals/mandelbrot.clj
@@ -54,21 +54,34 @@
 ;;             (bit-shift-left g 8)
 ;;             b)))
 
-
 (defn do-color
   "Maps a new color with the luminance based on the number of iterations it took
   for the point to escape the set. The plain mapping is rather dull, so here we
   focus on the points which are in the boundaries (intuitively, those that took
-  long to escape, but not longest). We calculate the luminance and saturation as
-  the distance to the middle value, which results in a nice 'electric' look
-  around the edges of the set"
-  [i, depth]
-  (let [ icorr (/ i (/ depth 100))
+  long to escape, but not longest). We calculate the luminance as the distance
+  to the middle value, which results in a nice 'electric' look around the edges
+  of the set"
+  [i depth]
+  (let [ icorr (/ (* i 100.0) depth)
         l (- 100 (* 2 (Math/abs (- 50 icorr))))
         [r g b] (c/hsl-to-rgb 205 l l)]
     (bit-or (bit-shift-left r 16)
             (bit-shift-left g 8)
             b)))
+;; (defn do-color
+;;   "Maps a new color with the luminance based on the number of iterations it took
+;;   for the point to escape the set. The plain mapping is rather dull, so here we
+;;   focus on the points which are in the boundaries (intuitively, those that took
+;;   long to escape, but not longest). We calculate the luminance and saturation as
+;;   the distance to the middle value, which results in a nice 'electric' look
+;;   around the edges of the set"
+;;   [i, depth]
+;;   (let [ icorr (/ i (/ depth 100))
+;;         l (- 100 (* 2 (Math/abs (- 50 icorr))))
+;;         [r g b] (c/hsl-to-rgb 205 l l)]
+;;     (bit-or (bit-shift-left r 16)
+;;             (bit-shift-left g 8)
+;;             b)))
 
 (defn do-ppm [points size depth name]
   (let [[x y] size
